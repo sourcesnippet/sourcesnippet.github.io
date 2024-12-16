ef81a9d1350fdcb1581710121cb14bb1db867243
ManasMakde <manasmakde@gmail.com>
2024-12-16 12:47:09 +0530

    Restructured file path more intuitively, Improved create site error logging, Reduced server complexity

--- _source/createsite.js
+++ _source/createsite.js
@@ -1,49 +1,64 @@
-import fs from 'fs';
-import path from 'path';
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
 import * as runtime from 'react/jsx-runtime'
 import { createElement } from 'react'
 import { renderToString } from 'react-dom/server'
 import { evaluate } from '@mdx-js/mdx'
 
-
 // Constants
-const mdx_root_directory = "./mdx";
-const site_root_directory = "../";
-const whitelist_folders = [".git", ".nojekyll"]
-const self_dir = path.basename(import.meta.dirname)
+export const SITE_ROOT_DIRECTORY = path.resolve("../")
+export const MDX_ROOT_DIRECTORY = path.resolve("../_mdx")
+export const STATIC_ROOT_DIRECTORY = path.resolve("../_static")
+const SELF_ROOT_DIRECTORY = path.resolve()
+const WHITELIST_FILE_NAMES = [
+    ".git",
+    ".nojekyll",
+    "ReadMe.md"
+]
+const WHITELIST_PATHS = [
+    MDX_ROOT_DIRECTORY,
+    STATIC_ROOT_DIRECTORY,
+    SELF_ROOT_DIRECTORY
+]
 
 
 // Functions
+function make_path_url_safe(str) {  // Makes the given string safe to pass as url
+    return str.toLowerCase().replaceAll(" ", "-")
+}
 async function mdx_to_html(mdx_code) {  // converts mdx code into html code
 
-    var base_url = import.meta.url // path.join(import.meta.dirname,"mdx");
+    const base_url = "file:///" + MDX_ROOT_DIRECTORY + "/"
     const jsx = (await evaluate(mdx_code, { ...runtime, baseUrl: base_url })).default
-    const html_code = renderToString(createElement(jsx));
+    const html_code = renderToString(createElement(jsx))
 
-    return html_code;
+    return html_code
 
 }
 function remove_old_directory() {  // Removed old site files
 
-    const files = fs.readdirSync(site_root_directory, { withFileTypes: true })
-
-
-    // Removes any folder which is not self and not in whitelist
+    // Removes any files and folder which are not whitelisted
+    const files = fs.readdirSync(SITE_ROOT_DIRECTORY, { withFileTypes: true })
     files.forEach(file => {
 
-        const is_whitelisted = whitelist_folders.includes(file.name)
-        const is_self = file.name === self_dir
+        let delete_path = path.resolve(path.join(file.parentPath, file.name))
+        const is_whitelisted = WHITELIST_FILE_NAMES.includes(file.name) || WHITELIST_PATHS.includes(delete_path)
 
-        if (!is_whitelisted && !is_self) {
-            let delete_path = path.join(file.parentPath, file.name)
-            fs.rmSync(delete_path, { recursive: true, force: true });
-        }
-    });
+        if (!is_whitelisted)
+            fs.rmSync(delete_path, { recursive: true, force: true })
+
+    })
 
 }
 function create_html_file(mdx_file_path) {  // Creates a respective html file for the given mdx file
 
     try {
+
+        // Exit if not of .mdx type
+        if (!mdx_file_path.endsWith("index.mdx"))
+            return
+
 
         // Get Mdx dir path and file name without extension
         let dir_path = path.dirname(mdx_file_path)
@@ -51,45 +66,36 @@ function create_html_file(mdx_file_path) {  // Creates a respective html file fo
         let file_name = path_parse.name
 
 
-        // Exit if not of .mdx type
-        if (path_parse.ext !== ".mdx")
-            return false;
-
-
-        // Exit if private
-        if (file_name.startsWith("_"))
-            return false
-
-
         // Get new Html file path
-        let relative_path = path.relative(mdx_root_directory, dir_path);
-        let html_file_name = file_name + ".html";
-        let html_path = path.join(site_root_directory, relative_path).toLowerCase()
-        let html_file_path = path.join(html_path, html_file_name).toLowerCase();
+        let relative_path = make_path_url_safe(path.relative(MDX_ROOT_DIRECTORY, dir_path))
+        let html_file_name = make_path_url_safe(file_name + ".html")
+        let html_path = path.join(SITE_ROOT_DIRECTORY, relative_path)
+        let html_file_path = path.join(html_path, html_file_name)
 
 
         // Read the mdx file and create it's respective html file
-        let data = fs.readFileSync(mdx_file_path, 'utf8');
+        let data = fs.readFileSync(mdx_file_path, 'utf8')
 
 
         // Convert MDX to Html
         mdx_to_html(data).then(html_content => {
 
             // Create directory
-            if (!fs.existsSync(html_path)) {
-                fs.mkdirSync(html_path, { recursive: true });
-            }
+            if (!fs.existsSync(html_path))
+                fs.mkdirSync(html_path, { recursive: true })
+
 
             // Write to file
-            fs.writeFileSync(html_file_path, html_content);
+            fs.writeFileSync(html_file_path, html_content)
 
+
+        }).catch(err => {
+            console.log(`Error while creating Html page "${html_file_path}" for "${mdx_file_path}":\n${err.stack}`)
         })
 
-        return true;
     }
     catch (err) {
         console.log(`Could not create Html page for "${mdx_file_path}", Error: ${err.stack}`)
-        return false;
     }
 
 }
@@ -104,32 +110,39 @@ function create_site_files(directory) {  // Recursively goes through mdx files a
         // Iterate through all directories
         files.forEach(file => {
 
-            const file_path = path.join(directory, file.name);
+            const file_path = path.join(directory, file.name)
 
-            if (file.isFile()) {
-                create_html_file(file_path);
-            }
-            else if (file.isDirectory() && file.name != self_dir) {
-                create_site_files(file_path, false);
-            }
+            if (file.isFile())
+                create_html_file(path.resolve(file_path))
 
-        });
+            else if (file.isDirectory())
+                create_site_files(file_path, false)
+
+        })
+
     }
     catch (err) {
+
         console.log(`Error while creating site, Error: ${err.stack}`)
+
     }
 
 }
-function create_site(directory = mdx_root_directory) {  // First Removes old site files, then creates new files
+function create_site(directory = MDX_ROOT_DIRECTORY) {  // First Removes old site files, then creates new files
 
     // Removing old html files
     remove_old_directory()
 
     // Creating new html files
-    create_site_files(directory);
+    create_site_files(directory)
 }
 
 
 // Create Site 
-create_site()
-console.log("Recreated Site!")
+let __filename = fileURLToPath(import.meta.url)
+let entryFile = process.argv?.[1];
+
+if (entryFile === __filename) {
+    create_site()
+    console.log("Recreated Site!")
+}
--- _source/server.js
+++ _source/server.js
@@ -1,17 +1,13 @@
-import fs from 'fs';
-import path from 'path';
-import { dirname } from 'path';
-import http from 'http';
-import { fileURLToPath } from 'url';
-import { fork } from 'child_process';
+import fs from 'fs'
+import path from 'path'
+import http from 'http'
+import { fork } from 'child_process'
+import { MDX_ROOT_DIRECTORY, SITE_ROOT_DIRECTORY } from './createsite.js'
 
 
 // Constants
-const PORT = 3000;
-const MDX_ROOT_DIRECTORY = "./mdx";
-const SITE_ROOT_DIRECTORY = "../";
-const STATIC_ROOT_DIRECTORY = "./static"
-const CREATE_SITE_FILE = "createsite.js"
+const PORT = 3000
+const CREATE_SITE_FILE = "./createsite.js"
 const MIME_TYPE = {
     '.html': 'text/html',
     '.css': 'text/css',
@@ -27,19 +23,13 @@ const MIME_TYPE = {
     '.svg': 'image/svg+xml',
     '.pdf': 'application/pdf',
     '.zip': 'application/zip',
-};
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+}
 
 
 // Functions
-function inside_directory(dir, path_name) {  // checks if given path is inside dir
-    let combined_path = path.join(__dirname, dir, path_name);
-    let path_exists = fs.existsSync(combined_path)
-    return path_exists;
-}
 function invoke_create_site() {  // Runs create site script via fork
-    fork(CREATE_SITE_FILE);
+    // invoking create_site() directly does not properly update the site hence using fork instead 
+    fork(CREATE_SITE_FILE) 
 }
 function watch_for_changes() {  // Watches MDX_ROOT_DIRECTORY files for any code change
 
@@ -47,71 +37,58 @@ function watch_for_changes() {  // Watches MDX_ROOT_DIRECTORY files for any code
 
         if (eventType == "change") {
             console.log(`File Changed: ${filename}`)
-            invoke_create_site();
+            invoke_create_site()
         }
 
-    });
+    })
 
 }
 function server_handle_req(req, res) {  // Handles incoming requests
 
     // Parse & Sanitize URL
-    let parsed_url = new URL("http://" + req.headers.host + req.url);
-    let sanitized_path = path.normalize(parsed_url.pathname).replace(/^(\.\.[\/\\])+/, '');
-    let final_path = ""
-
-
-    // Get inside which directory
-    let is_path_directory = !Boolean(path.parse(sanitized_path).ext)
-    let is_inside_site = inside_directory(SITE_ROOT_DIRECTORY, sanitized_path)
-    let is_inside_static = is_inside_site ? false : inside_directory(path.basename(STATIC_ROOT_DIRECTORY), sanitized_path)
-
-
-    // If inside site
-    if (is_inside_site) {
-        final_path = path.join(SITE_ROOT_DIRECTORY, sanitized_path, is_path_directory ? "/index.html" : "");
-    }
-
-    // If inside site folder
-    else if (is_inside_static) {
-        final_path = path.join("./", sanitized_path) //path.join(static_root_directory, sanitized_path);
-    }
-
+    let parsed_url = new URL("http://" + req.headers.host + req.url)
+    let sanitized_url = path.normalize(parsed_url.pathname).replace(/^(\.\.[\/\\])+/, '')
+    let is_directory = !Boolean(path.parse(sanitized_url).ext)
+    let relative_file_path = path.normalize(sanitized_url + (is_directory ? "/index.html" : ""))
+    let absolute_file_path = path.join(SITE_ROOT_DIRECTORY, relative_file_path)
+    let path_exists = fs.existsSync(absolute_file_path)
 
     // Responde with content of file
-    if (fs.existsSync(final_path))
+    if (path_exists)
         // read file from file system
-        fs.readFile(final_path, function (err, data) {
+        fs.readFile(absolute_file_path, function (err, data) {
             if (err) {
-                res.statusCode = 500;
-                res.end(`Error getting the file: ${err}.`);
+                res.statusCode = 500
+                res.end(`Error getting the file: ${err}.`)
             }
             else {
                 // Based on the URL path, extract the file extention. e.g. .js, .doc, ...
-                const ext = path.parse(final_path).ext;
-                res.setHeader('Content-type', MIME_TYPE[ext] || 'text/plain'); // if the file is found, set Content-type and send data
-                res.end(data);
+                const ext = path.parse(absolute_file_path).ext
+                res.setHeader('Content-type', MIME_TYPE[ext] || 'text/plain') // if the file is found, set Content-type and send data
+                res.end(data)
             }
 
-        });
+        })
     else {
-        res.statusCode = 404;
-        res.end(`404 Invalid url not found!`);
+        res.statusCode = 404
+        res.end(`404 Invalid url not found!`)
     }
 
 }
 function start_server() {  // Starts server at given port
 
     // Start Server
-    const server = http.createServer(server_handle_req);
+    const server = http.createServer(server_handle_req)
     server.listen(PORT, () => {
         console.log(`Watching for changes at directory "${MDX_ROOT_DIRECTORY}" & Listening at ${PORT} ...`)
-    });
+    })
 
 }
 
+
 // Recreate site
 invoke_create_site()
+
 
 // Start server and watch for changes if flag is passed
 watch_for_changes()

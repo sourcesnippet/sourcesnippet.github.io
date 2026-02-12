7219c6d4cb8fc64ce637f719f9b049a93d33ec6d
ManasMakde <manasmakde@gmail.com>
2026-02-12 20:58:25 +0530

    Added default urls to /all page

--- host-mdx.js
+++ host-mdx.js
@@ -2,6 +2,8 @@ import fs from "fs"
 import path from "path"
 import remarkHeadingId from 'remark-heading-id';
 import rehypeMdxCodeProps from 'rehype-mdx-code-props'
+import { parseHTML } from 'linkedom';
+import { SNIPPETS_PER_PAGE, SNIPPET_LIST_ID, addSnippetsToList } from "./snippets/script.js"
 
 
 // To Set Properties
@@ -11,6 +13,7 @@ const SNIPPETS_PER_FILE = 5;
 const SNIPPETS_DATA_DIR = "/static/data/"
 const SNIPPETS_DATA_PREFIX = "data-"
 const SNIPPETS_STATS_FILE = "_stats.json"
+const SNIPPETS_ALL_FILE = "/snippets/index.html"
 
 
 // Properties
@@ -42,6 +45,77 @@ function isSubPath(potentialParent, thePath) {
             thePath[potentialParent.length] === path.sep ||
             thePath[potentialParent.length] === undefined
         );
+}
+
+
+// Primary Methods
+function createNojekyll(outputPath) {
+    fs.writeFileSync(path.join(outputPath, '.nojekyll'), "");
+}
+function createSnippetsData(outputPath) {
+    // Sort snippets
+    snippetsList.sort((a, b) => {
+        // Sort by Date
+        const dateA = a.createdOnDate instanceof Date ? a.createdOnDate.getTime() : 0;
+        const dateB = b.createdOnDate instanceof Date ? b.createdOnDate.getTime() : 0;
+        if (dateA !== dateB) {
+            return dateA - dateB;
+        }
+
+
+        // Fallback sort by title
+        const titleA = (a.title || "").toLowerCase();
+        const titleB = (b.title || "").toLowerCase();
+        return titleA.localeCompare(titleB);
+    });
+
+
+    // Create data folder
+    let absDataDir = path.join(outputPath, SNIPPETS_DATA_DIR);
+    if (!fs.existsSync(absDataDir)) {
+        fs.mkdirSync(absDataDir);
+    }
+
+
+    // Create site data.json files
+    for (let i = 0; i < snippetsList.length; i += SNIPPETS_PER_FILE) {
+        const chunk = snippetsList.slice(i, i + SNIPPETS_PER_FILE);
+        const jsonContent = JSON.stringify({ snippets: chunk });
+        const fileNumber = (i / SNIPPETS_PER_FILE) + 1;
+        const fileName = `${SNIPPETS_DATA_PREFIX}${fileNumber}.json`;
+        fs.writeFileSync(path.join(absDataDir, fileName), jsonContent);
+    }
+
+
+    // Create stats file
+    let stats = {
+        snippetsCount: snippetsList.length,
+        snippetsPerFile: SNIPPETS_PER_FILE
+    }
+    fs.writeFileSync(path.join(outputPath, SNIPPETS_DATA_DIR, SNIPPETS_STATS_FILE), JSON.stringify(stats));
+}
+function injectDefaultDataIntoPrimary(outputPath) {
+    console.log("SNIPPETS_PER_PAGE=>", SNIPPETS_PER_PAGE)
+
+    // Get Html
+    const htmlFilePath = path.join(outputPath, SNIPPETS_ALL_FILE)
+    const html = fs.readFileSync(htmlFilePath, 'utf-8');
+    const { document } = parseHTML(html);
+
+
+    // Skip if list container not found
+    const listElement = document.querySelector(SNIPPET_LIST_ID);
+    if (!listElement) {
+        return;
+    }
+
+
+    // Add all snippets
+    addSnippetsToList(snippetsList, listElement, document);
+
+
+    // Update file
+    fs.writeFileSync(htmlFilePath, document.toString());
 }
 
 
@@ -89,7 +163,10 @@ export function onFileCreateEnd(inputPath, outputPath, inFilePath, outFilePath, 
     let absSnippetsDir = path.join(inputPath, SNIPPETS_DIR)
     let inputFileName = path.basename(inFilePath);
     if (result?.exports?.metaData && inputFileName == SNIPPETS_INDEX_FILE && isSubPath(absSnippetsDir, inFilePath)) {
-        snippetsList.push(result?.exports?.metaData);
+        snippetsList.push({
+            ...result?.exports?.metaData,
+            url: `/${path.dirname(path.relative(inputPath, inFilePath))}/`
+        });
     }
 }
 
@@ -101,49 +178,15 @@ export function onSiteCreateEnd(inputPath, outputPath, wasInterrupted) {
 
 
     // Create .nojekyll
-    fs.writeFileSync(path.join(outputPath, '.nojekyll'), "");
+    createNojekyll(outputPath)
 
 
-    // Sort snippets
-    snippetsList.sort((a, b) => {
-        // Sort by Date
-        const dateA = a.createdOnDate instanceof Date ? a.createdOnDate.getTime() : 0;
-        const dateB = b.createdOnDate instanceof Date ? b.createdOnDate.getTime() : 0;
-        if (dateA !== dateB) {
-            return dateA - dateB;
-        }
+    // Create data.json & _stats.json file for all snippets
+    createSnippetsData(outputPath);
 
 
-        // Fallback sort by title
-        const titleA = (a.title || "").toLowerCase();
-        const titleB = (b.title || "").toLowerCase();
-        return titleA.localeCompare(titleB);
-    });
-
-
-    // Create data folder
-    let absDataDir = path.join(outputPath, SNIPPETS_DATA_DIR);
-    if (!fs.existsSync(absDataDir)) {
-        fs.mkdirSync(absDataDir);
-    }
-
-
-    // Create site data.json files
-    for (let i = 0; i < snippetsList.length; i += SNIPPETS_PER_FILE) {
-        const chunk = snippetsList.slice(i, i + SNIPPETS_PER_FILE);
-        const jsonContent = JSON.stringify({ snippets: chunk });
-        const fileNumber = (i / SNIPPETS_PER_FILE) + 1;
-        const fileName = `${SNIPPETS_DATA_PREFIX}${fileNumber}.json`;
-        fs.writeFileSync(path.join(absDataDir, fileName), jsonContent);
-    }
-
-
-    // Create stats file
-    let stats = {
-        snippetsCount: snippetsList.length,
-        snippetsPerFile: SNIPPETS_PER_FILE
-    }
-    fs.writeFileSync(path.join(outputPath, SNIPPETS_DATA_DIR, SNIPPETS_STATS_FILE), JSON.stringify(stats));
+    // Inject default data into /all
+    injectDefaultDataIntoPrimary(outputPath);
 }
 
 export function toTriggerRecreate(event, path) {
@@ -154,4 +197,3 @@ export function toTriggerRecreate(event, path) {
 
     return true;
 }
-
--- snippets/script.js
+++ snippets/script.js
@@ -1,7 +1,9 @@
-console.log("hello world from snippets")
+// Properties
+export const SNIPPETS_PER_PAGE = 100;
+export const SNIPPET_LIST_ID = "#raw-snippet-list"
 
-const snippetsPerPage = 100;
 
+// Methods
 async function FetchSnippetsData(skipCount = 0, statsFilePath = '/static/data/_stats.json', dataFilePathPrefix = "/static/data/data-") {
     // Retrieve the stats file
     const statsResponse = await fetch(statsFilePath);
@@ -25,7 +27,7 @@ async function FetchSnippetsData(skipCount = 0, statsFilePath = '/static/data/_s
         for (let j = skiptoIndex; j < data.snippets.length; j++) {
 
             // Break if snippets required per page are exceeded
-            if (snippetsPerPage <= snippets.length) {
+            if (SNIPPETS_PER_PAGE <= snippets.length) {
                 break;
             }
 
@@ -38,7 +40,7 @@ async function FetchSnippetsData(skipCount = 0, statsFilePath = '/static/data/_s
 
 
         // If snippets summoned has exceeded required then break
-        if (snippetsPerPage <= snippets.length) {
+        if (SNIPPETS_PER_PAGE <= snippets.length) {
             break;
         }
     }
@@ -47,5 +49,51 @@ async function FetchSnippetsData(skipCount = 0, statsFilePath = '/static/data/_s
     return snippets;
 }
 
-// Execute the function
-FetchSnippetsData(100);
+async function RemoveChildren(element){
+    while (element.firstChild) {
+        element.removeChild(element.lastChild);
+    }
+}
+
+export function addSnippetsToList(snippets, listElement, doc){
+    // Add elements to list
+    const fragment = doc.createDocumentFragment();
+    for (let i = snippets.length - 1; i >= 0; i--){
+
+        // Create elements
+        const li = doc.createElement("li");
+        const a = doc.createElement("a");
+
+
+        // Add title and link
+        a.textContent = snippets[i].title;
+        a.href = snippets[i].url;
+        
+        
+        // Add element to parent
+        li.appendChild(a);
+        fragment.appendChild(li);
+    }
+
+    listElement.appendChild(fragment);
+}
+
+async function Main() {
+    // Get all snippets
+    let snippets = await FetchSnippetsData();
+
+
+    // Clear list
+    const listElement = document.querySelector(SNIPPET_LIST_ID);
+    RemoveChildren(listElement);
+
+
+    // Add to list
+    addSnippetsToList(snippets, listElement, document);
+}
+
+
+// To ensure this only runs in browser
+if (typeof window !== "undefined") {
+    Main();
+}
